ca1366df93506535ea285310d73c38e7682a3c0f
tumaolva <tumaolva@jyu.fi>
2023-12-18 22:14:14 +0200

    1.8-1.9 valmiita

--- Osa 1/Unicafe/src/App.jsx
+++ Osa 1/Unicafe/src/App.jsx
@@ -5,33 +5,54 @@ const Button = ({ handleClick, text }) => (
     {text}
   </button>
 )
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  if (total === 0) {
+    return (
+    <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </div>
+    );
+  }
+  const average = (good * 1 + neutral * 0 + bad * -1) / total;
+  const percentageGood = (good / total) * 100 || 0;
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {isNaN(average) ? 0 : average}</p>
+      <p>positive feedback {isNaN(percentageGood) ? 0 : percentageGood}%</p>
+    </div>
+  );
+};
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = (good * 1 + neutral * 0 + bad * -1)/ total
-  const percentageGood = good / total * 100 || 0;
 
-
-  const goodReview = () => {
-    
+  const goodReview = () => {    
     const updatedGood = good + 1
     setGood(updatedGood)
-    console.log('good feedback given. previous total' , good , 'new total', updatedGood)
+   // console.log('good feedback given. previous total' , good , 'new total', updatedGood)
   }
 
-  const neutralReview = () => {
-  
+  const neutralReview = () => {  
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    console.log('neutral feedback given. previous total' , neutral , 'new total', updatedNeutral)
+   // console.log('neutral feedback given. previous total' , neutral , 'new total', updatedNeutral)
   }
   const badReview = () => {  
     const updatedBad = bad + 1
     setBad(updatedBad)
-    console.log('bad feedback given. previous total' , bad , 'new total', updatedBad)
+   // console.log('bad feedback given. previous total' , bad , 'new total', updatedBad)
   }
 
   return (
@@ -45,15 +66,7 @@ const App = () => {
       <Button handleClick={badReview} text="bad" />
       </div>
       <div>
-        <h1>statistics</h1>
-      </div>
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>total {total}</p>
-        <p>average {isNaN(average) ? 0 : average}</p>
-        <p>positive {isNaN(percentageGood) ? 0 : percentageGood}%</p>
+       <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
