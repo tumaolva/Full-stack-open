2d5065ef241f38d9b0f2673f3b0f6e1c1cc620ca
tumaolva <tumaolva@jyu.fi>
2023-12-20 19:54:19 +0200

    osa 2.c harjoitukset, sekä tehtävä 2.11 valmis

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FilterForm from './components/filterForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 import PersonForm from './components/personForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  const hook = () => {
+    console.log('Odottaa...')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Onnistui!')
+        setPersons(response.data)
+      })
+  } 
+  useEffect(hook, [])
+
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
