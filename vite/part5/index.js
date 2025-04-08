db59d9692ac38f268dc8e6c762f8b5d05ee4465a
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-04-08 22:35:15 +0300

    front ja back yhteistoiminnallisuus varmistettu

--- vite/part5/index.js
+++ vite/part5/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
-
 
 
 let notes = [
@@ -37,18 +38,15 @@ let notes = [
   })
   
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
       response.json(note)
-    } else {
-      response.status(404).end()
-    }
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -74,15 +72,14 @@ let notes = [
       })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
       important: body.important || false,
-      id: generateId(),
-    }
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
 
   const unknownEndpoint = (request, response) => {
@@ -91,7 +88,7 @@ let notes = [
   
   app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
