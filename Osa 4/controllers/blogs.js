2b8807c50a814cf1770773df89f106031e5d7729
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-05-12 21:16:05 +0300

    4.18 ja 4.19 valmiita

--- Osa 4/controllers/blogs.js
+++ Osa 4/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 require('express')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,10 +11,22 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-  const user = await User.findById(body.userId)
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
   }
