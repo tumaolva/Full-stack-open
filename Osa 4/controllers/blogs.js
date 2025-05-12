594c63ff8fc750b2b315fda8b280459712632222
Tuomas Vainio <tumaolva@student.jyu.fi>
2025-05-12 21:02:31 +0300

    4.17 valmis

--- Osa 4/controllers/blogs.js
+++ Osa 4/controllers/blogs.js
@@ -1,17 +1,32 @@
 const blogsRouter = require('express').Router()
 require('express')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
+  const blog = new Blog({
+    ...body,
+    user: user._id, // liitetään blogi käyttäjään
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+    
   response.status(201).json(savedBlog)
     .catch(error => next(error))
 })
